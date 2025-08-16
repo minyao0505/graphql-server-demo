d28d7fc1b4f026d46c9322988ce73d55311a4231
minyao0505 <minyao0505@gmail.com>
2025-08-16 22:37:31 +0800

    start server

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 8000 },
+  });
+
+  console.log(`🚀 Apollo Server is running at ${url}`);
+};
+
+startServer();
