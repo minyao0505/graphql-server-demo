2afa6a7cb6f8365400e8a934f52eaaf5bb344e77
minyao0505 <minyao0505@gmail.com>
2025-08-17 20:43:45 +0800

    config codegen

---
+++ codegen.ts
@@ -0,0 +1,14 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import { defineConfig } from "@eddeee888/gcg-typescript-resolver-files";
+
+const config: CodegenConfig = {
+  schema: "src/**/schema.graphql",
+  hooks: {
+    afterAllFileWrite: ["prettier --write"],
+  },
+  generates: {
+    "src/schema": defineConfig(),
+  },
+};
+
+export default config;
