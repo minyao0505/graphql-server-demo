23d07cc67177e9cf4d9bc4f98b2f7069cba42ab5
minyao0505 <minyao0505@gmail.com>
2025-08-17 20:44:56 +0800

    create user schema

--- src/schema/user/resolvers/User.ts
+++ src/schema/user/resolvers/User.ts
@@ -1,17 +1,13 @@
-import type { UserResolvers } from "../../types";
+import type { UserResolvers } from "./../../types.generated";
 
 export const User: UserResolvers = {
   /* Implement User resolver logic here */
   productSold: async (parent, __, { data }) => {
-    const products = await data.$products.findBySeller({
-      id: String(parent.id),
-    });
+    const products = await data.$products.findBySeller({ id: parent.id });
     return products;
   },
   productPurchased: async (parent, __, { data }) => {
-    const products = await data.$products.findByBuyer({
-      id: String(parent.id),
-    });
+    const products = await data.$products.findByBuyer({ id: parent.id });
     return products;
   },
 };
